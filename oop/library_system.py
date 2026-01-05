ff1ac1a05d7a7e502c22633ca2350878fd4c0ad5
Danmuumbi <muuomuumbi@gmail.com>
2026-01-05 11:14:47 +0300

    oop project

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,21 +1,25 @@
+# Base class
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
+# Derived class for EBook
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(title, author)
-        self.file_size = file_size
+        super().__init__(title, author)  # Call base class initializer
+        self.file_size = file_size       # Unique attribute
 
+# Derived class for PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title, author)
-        self.page_count = page_count
+        super().__init__(title, author)  # Call base class initializer
+        self.page_count = page_count     # Unique attribute
 
+# Library class using composition
 class Library:
     def __init__(self):
-        self.books = []
+        self.books = []  # List to hold all books
 
     def add_book(self, book):
         self.books.append(book)
