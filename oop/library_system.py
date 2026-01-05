39a1740ae142ddd8a258f89c4d6bec0808965d30
Danmuumbi <muuomuumbi@gmail.com>
2026-01-05 11:15:53 +0300

    oop project

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,31 +4,35 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 # Derived class for EBook
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(title, author)  # Call base class initializer
-        self.file_size = file_size       # Unique attribute
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 # Derived class for PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title, author)  # Call base class initializer
-        self.page_count = page_count     # Unique attribute
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 # Library class using composition
 class Library:
     def __init__(self):
-        self.books = []  # List to hold all books
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(str(book))
